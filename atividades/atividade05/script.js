9d82269ad35fad92aa8d069396d31f564d86a475
Victor Freire Barros <victorfb54321@gmail.com>
2025-09-08 11:13:42 -0300

    progresso

---
+++ atividades/atividade05/script.js
@@ -0,0 +1,9 @@
+let nota01 = prompt("Informe sua 1° nota");
+let nota02 = prompt("Informe sua 2° nota");
+let nota03 = prompt("Informe sua 3° nota");
+let nota04 = prompt("Informe sua 4° nota");
+media = nota01+nota02+nota03+nota04/4
+if (media >=7){
+alert("Você foi aprovado com a media: ");
+
+}
---
+++ aulas/aula03/script.js
@@ -0,0 +1,8 @@
+let nome = prompt("Informe seu nome: ");
+let idade = prompt("Informe sua idade: ");
+if (idade >= 18){
+alert("Maior de idade: "+nome+ " Idade: "+idade);
+}
+else{
+alert("Menor de idade: "+nome+ " Idade: "+idade);
+}
